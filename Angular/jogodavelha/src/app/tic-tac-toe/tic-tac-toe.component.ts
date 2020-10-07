73237190deeb1e96f1761068ba5ca1931eec102c
Cinthia <49377882+CINPIS@users.noreply.github.com>
2020-10-07 13:34:50 -0300

    Add files via upload

---
+++ Angular/jogodavelha/src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -0,0 +1,78 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tic-tac-toe',
+  templateUrl: './tic-tac-toe.component.html',
+  styleUrls: ['./tic-tac-toe.component.css']
+})
+export class TicTacToeComponent {
+
+  currentPlayer: string = 'X';
+  winner: string = '';
+  matrizBoard: string[][] = [
+    ['','',''],
+    ['','',''],
+    ['','','']
+  ];
+
+  jogoDaVez(line: number, col: number){
+    //window.alert(`Jogo da Vez foi na linha: ${line} e na coluna: ${col} do jogador ${this.currentPlayer}`);
+    
+    if(this.matrizBoard[line][col] == '' && this.winner == ''){
+      this.matrizBoard[line][col] = this.currentPlayer;
+      
+      //winner?
+      if (this.ganhador(this.currentPlayer)){
+        this.winner = this.currentPlayer;
+      }
+
+      if(this.currentPlayer == 'X'){
+        this.currentPlayer = 'O'
+      } else {
+        this.currentPlayer = 'X';
+      }
+    }
+  }
+
+  ganhador(player: string): boolean{
+    for(let i = 0; i < this.matrizBoard.length; i++){
+      if(this.matrizBoard[i][0] == player 
+        && this.matrizBoard[i][1] == player
+        && this.matrizBoard[i][2] == player){
+          return true;
+        }
+    }
+    for(let i = 0; i < this.matrizBoard.length; i++){
+      if(this.matrizBoard[0][i] == player 
+        && this.matrizBoard[1][i] == player
+        && this.matrizBoard[2][i] == player){
+          return true;
+        }
+    }
+
+    //diagonal
+    if(this.matrizBoard[0][0] == player 
+      && this.matrizBoard[1][1] == player
+      && this.matrizBoard[2][2] == player){
+        return true;
+      }
+    if(this.matrizBoard[0][2] == player 
+      && this.matrizBoard[1][1] == player
+      && this.matrizBoard[2][0] == player){
+        return true;
+      }
+
+    return false;
+  }
+
+  reset() { 
+    this.currentPlayer = 'X';
+    this.winner = '';
+    this.matrizBoard = [
+      ['','',''],
+      ['','',''],
+      ['','','']
+    ];
+  }
+
+}
