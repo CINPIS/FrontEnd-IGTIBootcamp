39abd0847b86b28f4cae6dca07145dc74c2aa8d4
Cinthia <49377882+CINPIS@users.noreply.github.com>
2020-10-08 17:03:10 -0300

    Add files via upload

--- Angular/jogodavelha/src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ Angular/jogodavelha/src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tic-tac-toe',
-  templateUrl: './tic-tac-toe.component.html',
-  styleUrls: ['./tic-tac-toe.component.css']
+  templateUrl: './tic-tac-toe.component.html'
 })
 export class TicTacToeComponent {
 
